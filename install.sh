264cfa401b034453b4c57d0ca5c06acc7a45e93b
anderson06 <andersoncampos.m@gmail.com>
2016-09-22 08:02:40 -0300

    Add installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+# clean
+rm -fr ~/.gitconfig
+rm -fr ~/.tmux.conf
+rm -fr ~/.vimrc
+
+# link
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
+ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
+ln -sv $DOTFILES_DIR/vimrc ~/.vimrc
