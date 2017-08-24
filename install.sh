2c611846ec8ac4abc10cf3b97ecdbdf6121b7c0d
anderson06 <andersoncampos.m@gmail.com>
2017-08-24 14:10:57 -0300

    Automate vim plugin manager installation

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
 ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 ln -sv $DOTFILES_DIR/vimrc ~/.vimrc
+
+# install vim plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
