61f69373aa303e350023b5f77fd9ca078efd5233
anderson06 <andersoncampos.m@gmail.com>
2017-08-26 21:45:45 -0300

    Automate icons installation

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
 ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 ln -sv $DOTFILES_DIR/vimrc ~/.vimrc
+
+# install fonts
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
