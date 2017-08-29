8094a3b2bd05be0065e5af895fcfbba066bda233
anderson06 <andersoncampos.m@gmail.com>
2017-08-29 13:02:28 -0300

    Automate vim plugins installation

--- install.sh
+++ install.sh
@@ -16,6 +16,9 @@ cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Co
 # install vim plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# install vim plugins
+vim -c "PlugInstall|qa"
+
 # install favorite terminal apps
 sudo apt-get update
 sudo apt-get install zsh tmux vim-gnome silversearcher-ag
