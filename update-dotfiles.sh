be4f6011d6c73f76c50c1319f537c31659c3411b
Anderson <anderson@miro.com>
2023-12-20 16:36:45 +0100

    Start using nvim

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,24 +1,24 @@
 # clean
-rm -fr ~/.gitconfig
-rm -fr ~/.tmux.conf
-rm -fr ~/.vimrc
-rm -fr ~/.tern-config
-rm -fr ~/.vim/after/plugin/after-load.vim
-rm -fr ~/.config/nvim/init.vim
-rm -fr ~/.config/kitty/kitty.conf
+# rm -fr ~/.gitconfig
+# rm -fr ~/.tmux.conf
+# rm -fr ~/.vimrc
+# rm -fr ~/.tern-config
+# rm -fr ~/.vim/after/plugin/after-load.vim
+rm -fr ~/.config/nvim/init.lua
+# rm -fr ~/.config/kitty/kitty.conf
 
 # create dirs
-mkdir -p ~/.vim/after/plugin/
+# mkdir -p ~/.vim/after/plugin/
 mkdir -p ~/.config/nvim/
-mkdir -p ~/.config/kitty/
-mkdir -p ~/.local/share/nvim/backup
+# mkdir -p ~/.config/kitty/
+# mkdir -p ~/.local/share/nvim/backup
 
 # link
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
-ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
-ln -sv $DOTFILES_DIR/vimrc ~/.vimrc
-ln -sv $DOTFILES_DIR/tern-config ~/.tern-config
-ln -sv $DOTFILES_DIR/after-load.vim ~/.vim/after/plugin/after-load.vim
-ln -sv $DOTFILES_DIR/init.vim ~/.config/nvim/init.vim
-ln -sv $DOTFILES_DIR/kitty.conf ~/.config/kitty/kitty.conf
+# ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
+# ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
+# ln -sv $DOTFILES_DIR/vimrc ~/.vimrc
+# ln -sv $DOTFILES_DIR/tern-config ~/.tern-config
+# ln -sv $DOTFILES_DIR/after-load.vim ~/.vim/after/plugin/after-load.vim
+ln -sv $DOTFILES_DIR/init.lua ~/.config/nvim/init.lua
+# ln -sv $DOTFILES_DIR/kitty.conf ~/.config/kitty/kitty.conf
