7cf6fe268f358cc44d474166db19779f056d3f3f
Anderson <anderson@miro.com>
2023-12-20 16:52:29 +0100

    Remove tern stuff

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -2,7 +2,6 @@
 # rm -fr ~/.gitconfig
 # rm -fr ~/.tmux.conf
 # rm -fr ~/.vimrc
-# rm -fr ~/.tern-config
 # rm -fr ~/.vim/after/plugin/after-load.vim
 rm -fr ~/.config/nvim/init.lua
 # rm -fr ~/.config/kitty/kitty.conf
@@ -18,7 +17,6 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
 # ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
 # ln -sv $DOTFILES_DIR/vimrc ~/.vimrc
-# ln -sv $DOTFILES_DIR/tern-config ~/.tern-config
 # ln -sv $DOTFILES_DIR/after-load.vim ~/.vim/after/plugin/after-load.vim
 ln -sv $DOTFILES_DIR/init.lua ~/.config/nvim/init.lua
 # ln -sv $DOTFILES_DIR/kitty.conf ~/.config/kitty/kitty.conf
