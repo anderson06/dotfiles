eba04b2956730aa3f9b7491c4a039d4a2879ed61
anderson06 <andersoncampos.m@gmail.com>
2023-12-23 19:56:58 +0100

    Remove unused commands

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,19 +1,13 @@
 # clean
 # rm -fr ~/.gitconfig
 # rm -fr ~/.tmux.conf
-# rm -fr ~/.vimrc
-# rm -fr ~/.vim/after/plugin/after-load.vim
 rm -fr ~/.config/nvim/init.lua
 
 # create dirs
-# mkdir -p ~/.vim/after/plugin/
 mkdir -p ~/.config/nvim/
-# mkdir -p ~/.local/share/nvim/backup
 
 # link
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
 # ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
-# ln -sv $DOTFILES_DIR/vimrc ~/.vimrc
-# ln -sv $DOTFILES_DIR/after-load.vim ~/.vim/after/plugin/after-load.vim
 ln -sv $DOTFILES_DIR/init.lua ~/.config/nvim/init.lua
