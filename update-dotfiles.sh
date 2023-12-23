a3ed7dcd7bf30d376c4e8b52b3e86a3738f56a39
anderson06 <andersoncampos.m@gmail.com>
2023-12-23 20:24:13 +0100

    Improve nvim config

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,13 +1,13 @@
-# clean
+# Setup neovim
+# rm -fr ~/.config/nvim
+mv ~/.config/nvim ~/nvim_backup
+mkdir -p ~/.config/nvim/
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sv $DOTFILES_DIR/nvim/ ~/.config/nvim/
+
 # rm -fr ~/.gitconfig
 # rm -fr ~/.tmux.conf
-rm -fr ~/.config/nvim/init.lua
-
-# create dirs
-mkdir -p ~/.config/nvim/
 
 # link
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
 # ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
-ln -sv $DOTFILES_DIR/init.lua ~/.config/nvim/init.lua
