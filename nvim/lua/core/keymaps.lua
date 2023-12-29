a788357b48a1d3e4daf478cee9576144880778ce
anderson06 <andersoncampos.m@gmail.com>
2023-12-29 14:03:35 +0100

    Add scroll mapping

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,2 +1,12 @@
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', {})
+vim.keymap.set('n', '<C-d>', '<C-d>zz', {})
+vim.keymap.set('n', '<C-u>', '<C-u>zz', {})
 
+-- " Mappings
+-- nmap <leader>ne :NERDTreeToggle<cr>
+-- nmap <leader>cp :let @+=expand("%")<cr>
+-- nnoremap <cr> :noh<cr>
+-- nnoremap <bs> <c-^>
+-- nmap <leader>T :enew<cr>
+-- nmap <leader>l :bnext<cr>
+-- nmap <leader>h :bprevious<cr>
