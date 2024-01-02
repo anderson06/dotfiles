8d33ebeb3d4123e95bc7078ea7ed4ab4fb0fc03b
Anderson <anderson@miro.com>
2024-01-02 13:06:55 +0100

    Fix nvim link script

--- link-nvim-config.sh
+++ link-nvim-config.sh
@@ -1,13 +1,7 @@
 # Link /nvim/ to ~/.config/nvim/
 
-# Create a backup of old configs
-# mv ~/.config/nvim ~/nvim_backup
-
 # Delete old files
 rm -fr ~/.config/nvim
-
-# Create dir if necessary
-mkdir -p ~/.config/nvim/
 
 # Link dirs
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
