ff7d007e07261816b0734c8f699dd009f8a3e2a9
Anderson <anderson@miro.com>
2024-01-09 15:47:15 +0100

    Create options file

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,48 +16,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
-
--- Set highlight on search
--- vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-vim.o.undodir = vim.fn.expand('~/.vim/undo-dir')
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
@@ -396,4 +354,5 @@ vim.o.relativenumber = true
 vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal toggle left<CR>', {})
 vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal toggle float<CR>', {})
 
+require("core.options")
 require("core.keymaps")
