f7ae9c913183f75b3c999410504b14598f46d401
Anderson <anderson@miro.com>
2024-01-11 20:21:01 +0100

    Add awesome keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,5 +1,4 @@
 -- " Mappings
--- See `:help vim.keymap.set()`
 
 -- Remove search highlights
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", {})
@@ -21,11 +20,29 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnos
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
+-- Copy current file path into clipboard
 vim.keymap.set("n", "<leader>cp", function()
   local path = vim.fn.expand("%")
   vim.fn.setreg("+", path)
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end)
 
+-- Move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in position when using J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep searched terms in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Do not lose copied text when replacing
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Replace in real time all ocurrences of string under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Give execution permission to current file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
