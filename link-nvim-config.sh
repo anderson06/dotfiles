e3bd5f1b14ae6ccbe0d9e6ac84f9f96e09136f0a
Anderson <anderson@miro.com>
2024-01-11 23:00:02 +0100

    Remove old comments

--- link-nvim-config.sh
+++ link-nvim-config.sh
@@ -6,10 +6,3 @@ rm -fr ~/.config/nvim
 # Link dirs
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sv $DOTFILES_DIR/nvim/ ~/.config/
-
-# rm -fr ~/.gitconfig
-# rm -fr ~/.tmux.conf
-
-# link
-# ln -sv $DOTFILES_DIR/gitconfig ~/.gitconfig
-# ln -sv $DOTFILES_DIR/tmux.conf ~/.tmux.conf
