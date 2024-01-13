7d0f794365ef89839d95f3d722ff806fa776b474
Anderson <anderson@miro.com>
2024-01-13 12:42:09 +0100

    Fix prettier formatting issue

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,12 +6,18 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.formatting.prettier,
       },
     })
 
-    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+    vim.keymap.set("n", "<leader>f", function()
+      vim.lsp.buf.format {
+        -- Never use tsserver for formatting
+        -- This avoids conflicts with prettier
+        filter = function(client) return client.name ~= "tsserver" end
+      }
+    end, {})
   end,
 }
