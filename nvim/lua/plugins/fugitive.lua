4d1341807f5015aeb84a0ce8cae5580f10145eea
Anderson <anderson@miro.com>
2024-02-12 09:54:01 +0100

    feat: skip auto validations on push

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -13,6 +13,6 @@ return {
 
     -- Git
     vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit -n -m \"", { noremap = false })
-    vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push -u origin HEAD<CR>", { noremap = false })
+    vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push -u --no-verify origin HEAD<CR>", { noremap = false })
   end,
 }
