8100287e92fc270e6cb8ad669d04cce8d2aae360
Anderson <anderson@miro.com>
2024-02-25 19:37:42 +0100

    Remove coverage by default from test command

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,11 +1,9 @@
 return {
   "vim-test/vim-test",
   config = function()
-    vim.keymap.set("n", "<leader>tn", function()
-      require("neotest").run.run(vim.fn.expand("%"))
-    end)
+    vim.g["test#strategy"] = "neovim"
 
-    vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>", {})
+    vim.keymap.set("n", "<leader>tn", ":TestNearest --coverage=false<CR>", {})
     vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", {})
     vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>", {})
     vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", {})
