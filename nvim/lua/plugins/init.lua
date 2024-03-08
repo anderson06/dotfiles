17bfb2321903ad71aee712a4e0f79d6dd3d78aef
Anderson <anderson@miro.com>
2024-03-08 14:58:08 +0100

    tpope plugins <3

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,12 +5,18 @@ return {
   -- Detect tabstop and shiftwidth automatically
   "tpope/vim-sleuth",
 
+  -- Add nice keymaps for []
+  "tpope/vim-unimpaired",
+
+  -- Maps edit suround chars
+  "tpope/vim-surround",
+
+  -- Super charge . command
+  "tpope/vim-repeat",
+
   -- "gc" to comment visual regions/lines
   { "numToStr/Comment.nvim", opts = {} },
 
   -- Make background transparent
   "xiyaowong/transparent.nvim",
-
-  -- Add nice keymaps for []
-  "tpope/vim-unimpaired",
 }
