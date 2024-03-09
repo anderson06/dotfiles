f94d33ccaddedfe3b27e27084e68204cf8621c5c
Anderson <anderson@miro.com>
2024-03-09 21:16:00 +0100

    Save file before running test

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,15 +1,15 @@
 return {
   "vim-test/vim-test",
   dependencies = {
-    'preservim/vimux',
+    "preservim/vimux",
   },
   config = function()
     vim.g["test#strategy"] = "vimux"
 
-    vim.keymap.set("n", "<leader>tn", ":TestNearest --coverage=false<CR>", {})
-    vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", {})
-    vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>", {})
-    vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", {})
-    vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>", {})
+    vim.keymap.set("n", "<leader>tn", ":w | TestNearest --coverage=false<CR>", { desc = "[T]est [N]earest" })
+    vim.keymap.set("n", "<leader>tf", ":w | TestFile<CR>", { desc = "[T]est [F]ile" })
+    vim.keymap.set("n", "<leader>ts", ":w | TestSuite<CR>", { desc = "[T]est [S]uite" })
+    vim.keymap.set("n", "<leader>tl", ":w | TestLast<CR>", { desc = "[T]est [L]ast" })
+    vim.keymap.set("n", "<leader>tv", ":w | TestVisit<CR>", { desc = "[T]est [V]isit" })
   end,
 }
