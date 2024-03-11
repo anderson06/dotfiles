646f48f9c5c595a121669bca48af0e316a383e51
Anderson <anderson@miro.com>
2024-03-11 10:49:46 +0100

    Remap ; to avoid conflicts with arrow plugin

--- nvim/lua/plugins/arrow.lua
+++ nvim/lua/plugins/arrow.lua
@@ -1,7 +1,9 @@
+vim.keymap.set("n", "<leader>;", ";", { noremap = false })
+
 return {
   "otavioschwanck/arrow.nvim",
   opts = {
     show_icons = true,
-    leader_key = ";", -- Recommended to be a single key
+    leader_key = ";",
   },
 }
