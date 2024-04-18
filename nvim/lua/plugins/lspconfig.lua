4da6922476820b47a9df467bd24c7de6128631a9
Anderson <anderson@miro.com>
2024-04-18 09:44:21 +0200

    Use confort for fomatting

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -9,15 +9,6 @@ local on_attach = function(_, bufnr)
     vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap("<leader>fm", function()
-    vim.lsp.buf.format({
-      -- Never use tsserver for formatting
-      -- This avoids conflicts with prettier
-      filter = function(client)
-        return client.name ~= "tsserver"
-      end,
-    })
-  end, "[F]ormat")
   nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
   nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
